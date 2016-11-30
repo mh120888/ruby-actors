bed4a398e82307bad0fd7a5c6fdf8d885382b39a
Matt Higgins <matthew.alan.higgins@gmail.com>
2016-11-30 14:23:35 -0800

    Replace Celluloid with custom Actor implementation

---
+++ chopstick.rb
@@ -0,0 +1,20 @@
+class Chopstick
+  def initialize
+    @mutex = Mutex.new
+  end
+
+  def take
+    @mutex.lock
+  end
+
+  def drop
+    @mutex.unlock
+
+  rescue ThreadError
+    puts "Trying to drop a chopstick not acquired"
+  end
+
+  def in_use?
+    @mutex.locked?
+  end
+end
---
+++ philosopher.rb
@@ -0,0 +1,52 @@
+require_relative 'actor'
+
+class Philosopher
+  include Actor
+
+  def initialize(name)
+    @name = name
+  end
+
+  def dine(table, position, waiter)
+    @waiter = waiter
+
+    @left_chopstick  = table.left_chopstick_at(position)
+    @right_chopstick = table.right_chopstick_at(position)
+
+    think
+  end
+
+  def think
+    puts "#{@name} is thinking"
+    sleep(rand)
+
+    @waiter.async.request_to_eat(Actor.current)
+  end
+
+  def eat
+    take_chopsticks
+
+    puts "#{@name} is eating."
+    sleep(rand)
+
+    drop_chopsticks
+
+    @waiter.async.done_eating(Actor.current)
+
+    think
+  end
+
+  def take_chopsticks
+    @left_chopstick.take
+    @right_chopstick.take
+  end
+
+  def drop_chopsticks
+    @left_chopstick.drop
+    @right_chopstick.drop
+  end
+
+  def finalize
+    drop_chopsticks
+  end
+end
